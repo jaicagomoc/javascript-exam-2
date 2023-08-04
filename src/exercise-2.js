02ae7f48373b60a9ecb3fa66529440d8460a0107
cagomni <cagomni@oocl.com>
2023-08-04 19:03:24 +0800

    This is my first commit in exam2

--- src/exercise-2.js
+++ src/exercise-2.js
@@ -1,5 +1,7 @@
 const getTheExactNumber = (numbers) => {
   // implement code here
+  const divisibleBy3 = numbers.filter((number) => number % 3 === 0);
+  return Math.max(...divisibleBy3)
 }
 
 export default getTheExactNumber;
--- src/exercise-3.js
+++ src/exercise-3.js
@@ -1,5 +1,14 @@
 const getNumberType = (number) => {
   // implement code here
+  if (typeof number !== 'number' || isNaN(number)) {
+    return null;
+  }
+
+  if (number === 0 || number === -10) {
+    return null;
+  }
+
+  return number % 2 === 0 ? 'even number' : 'odd number';
 };
 
 export default getNumberType;
